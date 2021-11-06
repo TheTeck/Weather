29c58463af1be4c20a5fd41666e5ab1a5fcb834e
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-06 14:10:24 -0700

    Create MainHeader layout with app title. Still need the search bar

---
+++ src/components/MainHeader/MainHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './MainHeader.scss';
+
+export default function MainHeader (props) {
+    return (
+        <div id="mainheader-container">
+            <p id="app-title">Weather App</p>
+        </div>
+    )
+}
