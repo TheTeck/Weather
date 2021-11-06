f39e4d8955d12027c480627d4093634426418919
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-06 15:40:46 -0700

    Create search bar component that will only allow numbers and no more than 5. Button click will lift the zip code value to the main app component

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './MainHeader.scss';
+import SearchBar from '../SearchBar/SearchBar';
 
 export default function MainHeader (props) {
     return (
         <div id="mainheader-container">
             <p id="app-title">Weather App</p>
+            <SearchBar getZipcode={props.getZipcode} />
         </div>
     )
 }
