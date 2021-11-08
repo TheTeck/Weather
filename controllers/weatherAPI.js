df3a97d2364c18d1ca98ac7bfd491df74d406b65
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-07 19:58:30 -0800

    Create controller and route for making an API call to get the more thorough data from Open Weather. Also set the icon and title  in the index html

--- controllers/weatherAPI.js
+++ controllers/weatherAPI.js
@@ -3,7 +3,8 @@ const request = require('request');
 const apiKey = process.env.WEATHER_KEY;
 
 module.exports = {
-    getCurrent
+    getCurrent,
+    getSevenDay
 };
 
 // Return the object from the API call with the zipcode provided as parameter
@@ -14,6 +15,22 @@ function getCurrent (req, res) {
             
             if (err) {
                 res.status(400).send({ message: "Invalid zipcode" });
+            }
+            
+            let data = JSON.parse(body);
+            res.send(data);
+        }
+    );
+}
+
+// Return the object from the One Call API using the lat and lon parameters
+function getSevenDay (req, res) {
+    request(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${req.params.lat}&lon=${req.params.lon}&appid=${apiKey}&units=imperial`,
+        function(err, response, body) {
+            
+            if (err) {
+                res.status(400).send({ message: "Invalid coordinates" });
             }
             
             let data = JSON.parse(body);
---
+++ src/dummyCurrent.js
@@ -0,0 +1,49 @@
+module.exports = {
+    "coord": {
+      "lon": -0.1257,
+      "lat": 51.5085
+    },
+  
+    "weather": [
+  
+      {
+        "id": 801,
+        "main": "Clouds",
+        "description": "few clouds",
+        "icon": "02n"
+      }
+    ],
+    "base": "stations",
+  
+    "main": {
+      "temp": 54.3,
+      "feels_like": 271.66,
+      "temp_min": 51.2,
+      "temp_max": 63.2,
+      "pressure": 1024,
+      "humidity": 90
+    },
+    "visibility": 10000,
+  
+    "wind": {
+      "speed": 2.57,
+      "deg": 200
+    },
+  
+    "clouds": {
+      "all": 12
+    },
+    "dt": 1636091095,
+  
+    "sys": {
+      "type": 2,
+      "id": 2019646,
+      "country": "GB",
+      "sunrise": 1636095672,
+      "sunset": 1636129618
+    },
+    "timezone": 0,
+    "id": 2643743,
+    "name": "Brentwood",
+    "cod": 200
+  }
