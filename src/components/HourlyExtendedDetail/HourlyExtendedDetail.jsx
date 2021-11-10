08efb6ff273e945b839ef5d11d4e5569dc89b3f3
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-10 09:11:05 -0800

    Add the responsive extra details for when user clicks on a particular hour on the hourly page

--- src/components/HourlyExtendedDetail/HourlyExtendedDetail.jsx
+++ src/components/HourlyExtendedDetail/HourlyExtendedDetail.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './HourlyExtendedDetail.scss';
 
-export default function HourlyExtendedDetail (props) {
+export default function HourlyExtendedDetail ({ icon, title, data }) {
     return (
         <div className="extendeddetail-container">
-
+            <div className="material-icons extra-icon">{icon}</div>
+            <div>
+                <div>{title}</div>
+                <div className="extra-data">{data}</div>
+            </div>
         </div>
     )
 }
