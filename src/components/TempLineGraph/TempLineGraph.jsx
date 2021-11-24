d78aba07336e9607ee3de20f8699ae827b90fead
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-23 22:00:36 -0800

    Create component to render a linegraph for hourly temps for the next 24 hours using D3

---
+++ src/components/TempLineGraph/TempLineGraph.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './TempLineGraph.scss';
+
+export default function TempLineGraph (props) {
+    return (
+        <div id="templinegraph-container">
+
+        </div>
+    )
+}
